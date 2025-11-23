b25fa3957dffb994f7a339fd91be6a5dd8cc3d09
CHANDAN-561 <chandansahuedu@gmail.com>
2025-11-23 13:26:09 +0530

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = false
+
+// console.log (typeof (score))
+// console.log(typeof score);   //both are the same
+let valueInNumber = Number (score)
+// console.log(typeof (valueInNumber));
+
+// console.log (valueInNumber)
+
+
+
+/* 
+"33" -> 33
+"33abc" -> NaN
+true -> 1 : false -> 0
+*/
+
+let isLoggedIn = "hello"
+
+let isLoggedInInBoolean = Boolean (isLoggedIn)
+// console.log(isLoggedInInBoolean);
+
+/*
+1 -> true : 0 -> false
+"" -> false
+"abc" -> true
+*/
+
+
+let number = 45
+let numberInString = String (number)
+console.log(typeof (numberInString));
+console.log(numberInString);
