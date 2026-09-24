945621b35fb139b377ac40aa1c76a98e250afbd4
CHANDAN-561 <chandansahuedu@gmail.com>
2026-09-24 22:21:39 +0530

    add different string methodes

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -2,4 +2,18 @@ const name = "Chandan";
 const age = 21;
 const pos = "Jr. dev";
 
+//old syntax for string concat
+console.log("Hello I am " + name + " age " + age + ", " + pos);
+
+//new syntax for string concat
 console.log(`Hello I am ${name} age ${age}, ${pos}`);
+
+//There are so many methodes/functions available for strings
+const dialouge = new String ("Hello"); //forcing the string to be initialize in heap rather than stack
+
+console.log(dialouge.charAt(2));  //returns the character at that index
+console.log(dialouge.indexOf('e')); //returns the index of the character, returns -1 if not present.
+console.log(dialouge.toUpperCase()); //turns the string to uppercase ofc.
+console.log(dialouge.toLowerCase()); //turns the string to lowercase.
+console.log(dialouge.startsWith('H')); //checks if the string starts with the character we pass, returns boolean value
+
