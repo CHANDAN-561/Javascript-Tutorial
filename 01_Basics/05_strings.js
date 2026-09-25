663c0b938ed8727387557bdb42e85ea5a5fd489f
CHANDAN-561 <chandansahuedu@gmail.com>
2026-09-25 22:29:39 +0530

    add more string method

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -13,7 +13,11 @@ const dialouge = new String ("Hello"); //forcing the string to be initialize in 
 
 console.log(dialouge.charAt(2));  //returns the character at that index
 console.log(dialouge.indexOf('e')); //returns the index of the character, returns -1 if not present.
-console.log(dialouge.toUpperCase()); //turns the string to uppercase ofc.
-console.log(dialouge.toLowerCase()); //turns the string to lowercase.
+console.log(dialouge.toUpperCase()); //turns the string to uppercase ofc
+console.log(dialouge.toLowerCase()); //turns the string to lowercase
 console.log(dialouge.startsWith('H')); //checks if the string starts with the character we pass, returns boolean value
+console.log(dialouge.includes("ello")); //returns true if the string contains the substring we are passing as an argument and vice-versa
+
+const userName = "   voyger    ONE     "
+console.log(userName.trim()); //this cuts out the leading spaces and starting spaces but not that are in between
 
